803f1e49f866ce9e9abbe027a816cc4a501cd741
gaamjaa <49315208+gaamjaa@users.noreply.github.com>
2022-08-14 19:13:43 +0900

    add DateItem

---
+++ src/component/DateItem.js
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
+const DateItem = ({id, tagName, sender, date}) => {
+  const navigate = useNavigate();
+  const getTag = () => {
+    navigate(`/view/${id}`);
+  };
+  return (
+    <div class="inline-flex flex-row">
+      <button
+        onClick={getTag}
+        class="h-24 w-32 m-2 border cursor-pointer whitespace-nowrap rounded-md px-5 py-3 bg-[#92C7DF] text-white shadow-md"
+      >
+        <div class="text-left">{sender}</div>
+        <br></br>
+        <div class="text-right">#{tagName}</div>
+      </button>
+    </div>
+  );
+};
+
+export default DateItem;
