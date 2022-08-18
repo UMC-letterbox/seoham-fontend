f22513cc73600621c27d8217f111ffe568b5021b
ariling <97501226+Ariling@users.noreply.github.com>
2022-08-18 18:58:23 +0900

    Update FindPW.js

--- src/Pages/FindPW.js
+++ src/Pages/FindPW.js
@@ -1,11 +1,16 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const FindPw = () => {
+  const navigate = useNavigate();
   const [inputPw, setInputPw] = useState("");
   const [inputEmail, setInputEmail] = useState("");
   const [inputAdmire, setInputAdmire] = useState("");
   const [newPw, setNewpw] = useState("");
+  const [isPassword, setIsPassword] = useState(false);
+  const [isEmail, setIsEmail] = useState(false);
+  const [isNumber, setIsNumber] = useState(false);
+  const [passwordMessage, setPasswordMessage] = useState("");
 
   const handleInputPw = (e) => {
     setInputPw(e.target.value);
@@ -30,7 +35,85 @@ const FindPw = () => {
   const onConfirm = () => {
     alert("일단은 맞다하자");
   };
-
+  const certifyEmail = (e) => {
+    e.preventDefault();
+    const { email_number } = inputEmail;
+    fetch("http://www.duke0410.shop:8000/user/check-email", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email_number,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.isSuccess == true ) {
+          alert("이메일이 유효합니다 인증번호를 전송했습니다");
+          setIsEmail(true);
+        } else {
+          alert("이메일이 유효하지 않거나 존재하지 않습니다.");
+        }
+      });
+  };
+  const certifyNumber = (e) => {
+  e.preventDefault();
+  const { email_number } = inputEmail;
+  const {certification_number} = inputAdmire;
+  fetch("API주소", {
+    method: "http://www.duke0410.shop:8000/user/code",
+    body: JSON.stringify({
+      authNum : certification_number,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.isSuccess == true) {
+        alert("인증번호가 맞습니다 비밀번호 변경을 해주세요");
+        setIsNumber(true);
+      } else {
+        alert("인증번호가 맞지 않습니다.");
+      }
+    });
+};
+  const onChangePassword = (e) => {
+    setInputPw(e.target.value);
+    const passwordRegExp =
+      /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+    if (!passwordRegExp.test(inputPw)) {
+      setPasswordMessage(
+        "숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요!"
+      );
+    } else {
+      setPasswordMessage("안전한 비밀번호 입니다.");
+      setIsPassword(true);
+    }
+  };
+  const onPwConfirm = () => {
+    if (isEmail === true && isNumber === true && isPassword === true && inputPw === newPw) {
+      fetch("http://www.duke0410.shop:8000/user/find", {
+        // 백엔드로 api호출!
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: inputEmail,
+          passWord: newPw,
+        }),
+      }).then((res) => {
+        if (res.isSuccess == true) {
+          alert("비밀번호 변경되었습니다.");
+          navigate("/login");
+        } else {
+          alert("조금 있다가 다시 시도해주십시오");
+        }
+      });
+    } else {
+      alert("비밀번호를 확인해주세요");
+    }
+  };
   return (
     <div>
       <h1 class="my-5 py-2 text-xl text-center">계정 / 비밀번호찾기</h1>
@@ -53,7 +136,7 @@ const FindPw = () => {
           onChange={handleInputEmail}
         />
         <button
-          onClick={onEmail}
+          onClick={certifyEmail}
           class="text-center border rounded-full text-red-300 w-1/4 border-red-300"
         >
           인증번호 전송
@@ -69,7 +152,7 @@ const FindPw = () => {
           onChange={handleInputAdmire}
         />
         <button
-          onClick={onConfirm}
+          onClick={certifyNumber}
           class="text-center border rounded-full text-red-300 w-1/4 border-red-300"
         >
           확인
@@ -97,14 +180,12 @@ const FindPw = () => {
         />
       </div>
       <div class="flex justify-center">
-        <Link to="/">
-          <button
-            onClick={onClick}
-            class="h-12 w-4/5 my-3 mx-10 border cursor-pointer rounded-full px-12 py-3 bg-[#FFB6C1] text-white"
-          >
-            비밀번호 변경
-          </button>
-        </Link>
+        <button
+          onClick={onPwConfirm}
+          class="h-12 w-4/5 my-3 mx-10 border cursor-pointer rounded-full px-12 py-3 bg-[#FFB6C1] text-white"
+        >
+          비밀번호 변경
+        </button>
       </div>
     </div>
   );
