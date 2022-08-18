71163286ced7ba124a99ab9c6a043012d0c670a6
ariling <97501226+Ariling@users.noreply.github.com>
2022-08-18 18:57:21 +0900

    Update FindId.js

--- src/Pages/FindId.js
+++ src/Pages/FindId.js
@@ -3,12 +3,62 @@ import { Link } from "react-router-dom";
 
 const FindId = () => {
   const [inputId, setInputId] = useState("");
-
+  const [isId, setIsId] = useState(false);
+  const [EmailMessage, setEmailMessage] = useState("");
   const handleInputId = (e) => {
     setInputId(e.target.value);
   };
   const onClick = () => {
     alert("확인되었습니다");
+  };
+  const onConfirm = () => {
+    alert("일단은 맞다하자");
+  };
+  const certifyId = (e) => {
+    e.preventDefault();
+    const { id_number } = inputId;
+    fetch("http://www.duke0410.shop:8000/user/check-email", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nickName : id_number,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.isSuccess == true) {
+          alert("닉네임이 유효합니다. 이메일 찾기를 눌러주세요");
+          setIsId(true);
+        } else {
+          alert("닉네임이 유효하지 않거나 존재하지 않습니다.");
+        }
+      });
+  };
+  const findEmail = () => {
+    const { id_number } = inputId;
+    if (isId === true) {
+      fetch("http://www.duke0410.shop:8000/user/find-nickname", {
+        method: "GET",
+        headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nickName : id_number,
+      }),
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          if (res.isSuccess == true ) {
+            setEmailMessage(`당신의 이메일은 ${res.data.email}입니다.`);
+          } else {
+            alert("이메일이 유효하지 않거나 존재하지 않습니다.");
+          }
+        });
+    } else {
+      alert("닉네임 확인을 먼저 해주세요");
+    }
   };
   return (
     <div>
@@ -31,17 +81,20 @@ const FindId = () => {
           onChange={handleInputId}
         />
         <button
-          onClick={onClick}
+          onClick={certifyId}
           class="text-center border rounded-full text-red-300 w-1/5 border-red-300"
         >
           확인
         </button>
       </div>
       <div class="flex justify-center">
-        <button class="h-12 w-4/5 my-3 mx-10 border cursor-pointer rounded-full px-12 py-3 bg-[#FFB6C1] text-white">
+        <button
+          onClick = {findEmail}
+          class="h-12 w-4/5 my-3 mx-10 border cursor-pointer rounded-full px-12 py-3 bg-[#FFB6C1] text-white">
           이메일 찾기
         </button>
       </div>
+      <p>{EmailMessage}</p>
     </div>
   );
 };
