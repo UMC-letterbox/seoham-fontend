114f276ee5f5ddaf418d3bdd9d508224190b4fc4
gaamjaa <dlgodus21@gmail.com>
2022-08-22 16:17:54 +0900

    태그 수정 페이지 추가

---
+++ src/Pages/Modi_tag.js
@@ -0,0 +1,95 @@
+import {useState} from "react";
+import {Link, useNavigate, useParams} from "react-router-dom";
+import * as Data from '../getTags';
+
+function Modi_tag() {
+    const {tagIdx} = useParams();
+    console.log("tag id: ", tagIdx);
+
+    const init_color = '#BF9270'
+    const init_name = '생일'
+    const userId = 0;
+
+    const [color, setColor] = useState(init_color);
+    const [tagName, setTagName] = useState(init_name);
+    const navigate = useNavigate();
+
+    const changeTag = (event) => setTagName(event.target.value);
+    const clickCheck = () => {
+        if (tagName.length === 0) {
+            window.alert("태그 이름을 입력해주세요");
+        }
+        else{
+            //
+            fetch(`/posts/tags/edit/${tagIdx}`, {
+                method: 'PATCH',
+                headers: {
+                    "Content-Type" : "application/json",
+                    Authorization: localStorage.getItem("login_token")
+                },
+                body: JSON.stringify({
+                    tagName: tagName,
+                    tagColor: color,
+                    userIdx: userId
+                })
+            })
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                if(res.isSuccess) {
+                    console.log(res);
+                    alert("저장 완료");
+                }
+            })
+            .catch(err => console.log(err))
+            //
+
+            navigate(-1);    
+        }
+    }
+
+
+
+    return (
+        <div>
+            <div className="flex justify-between m-5">
+                <Link to={'/'}>
+                    <button>
+                        <img src="/img/close.png" className="w-4 h-4"/>
+                    </button>
+                </Link>
+                <p className="font-bold text-2xl">TAG</p>
+                <button>
+                    <img src="/img/check-green.png" className="w-6 h-6" onClick={clickCheck}/>
+                </button>
+            </div>
+            <div className="body flex justify-center mt-10">
+                <div
+                    style={{backgroundColor: color}}
+                    className="grid place-content-center w-8/12 h-52 rounded-md m-10 shadow-lg" 
+                >
+                    <p className="font-bold text-lg">#{tagName}</p>
+                </div>
+            </div>
+            <div className="inputbox">
+                <div className="flex justify-center m-3">
+                    <span className="text-zinc-400">태그 이름</span>
+                    <input type="text" className="border-b-4 w-6/12 ml-5" onChange={changeTag} value={tagName}/>
+                </div>
+                <div className="flex justify-center mx-3 mb-3 mt-8">
+                    <span className="text-zinc-400">태그 색상</span>
+                    <div className="flex justify-around w-6/12 ml-5">
+                        <button className="rounded-md bg-[#BF9270] w-6 h-6" onClick={()=>{setColor('#BF9270');}}/>
+                        <button className="rounded-md bg-[#E3B7A0] w-6 h-6" onClick={()=>{setColor("#E3B7A0")}}/>
+                        <button className="rounded-md bg-[#EDCDBB] w-6 h-6" onClick={()=>{setColor("#EDCDBB")}}/>
+                        <button className="rounded-md bg-[#FCE2DB] w-6 h-6" onClick={()=>{setColor("#FCE2DB")}}/>
+                        <button className="rounded-md bg-[#FFEDDB] w-6 h-6" onClick={()=>{setColor("#FFEDDB")}}/>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Modi_tag;
