8b63b4e6a4180dbcb5a602e3ce65d0e48c125145
gaamjaa <dlgodus21@gmail.com>
2022-08-24 03:18:32 +0900

    태그별 편지 css 수정

--- src/Components/Tags_hy.js
+++ src/Components/Tags_hy.js
@@ -6,13 +6,19 @@ function Tags({id, name, letters, color}) {
         <div className="effect">
             {
                 id === -1 ? 
-                <button className="w-32 h-20 bg-gray-200 m-3 font-bold rounded-md shadow-md">
-                    <Link to={`/newTag`}><p className="text-3xl">+</p></Link>
+                <button className="w-32 h-20 bg-[#BABABA] m-3 font-bold rounded-md shadow-md">
+                    <Link to={`/newTag`}>
+                        <span className="flex justify-center">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2 12.1074H22M12 2V22" stroke="white" stroke-width="4" stroke-linecap="round"/>
+                        </svg>
+                        </span>
+                    </Link>
                 </button>
                 :
                 <button 
                     style={{backgroundColor: color}}
-                    className="w-32 h-20 m-3 font-bold rounded-md shadow-md"
+                    className="grid place-items-start w-32 h-20 m-3 p-2 font-bold rounded-md shadow-md buri text-white text-sm"
                 >
                     <Link to={`/tags/${id}`}>#{name}</Link>
                 </button>
