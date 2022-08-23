5800a0e33b6485defda672155d2e2f67cede9423
jjeongsu <92345556+jjeongsu@users.noreply.github.com>
2022-08-23 22:57:39 +0900

    Update MyButton3.js

--- src/Components/MyButton3.js
+++ src/Components/MyButton3.js
@@ -1,13 +1,17 @@
-//얘만 살짝 다른 버전
 const MyButton3 = ({ text, onClick }) => {
   return (
     <button
-      class="upBtn border border-zinc-400 cursor-pointer rounded-full font-bold h-10 w-10 fixed right-2 bottom-2 text-zinc-400 bg-white"
+      class="flex justify-center items-center upBtn border border-zinc-400 cursor-pointer rounded-full font-bold h-10 w-10 fixed right-2 bottom-2 text-zinc-400 bg-white"
       onClick={onClick}
     >
-      {text}
+      <svg 
+      className="w-5" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.5 11.1074H21.5M11.5 1V21" stroke="#989898" strokeWidth="2" stroke-linecap="round"/>
+      </svg>
+
     </button>
   );
 };
 
 export default MyButton3;
+
