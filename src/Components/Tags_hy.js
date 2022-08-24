0313105b2ce6f31009e3160179e2c35eaac0ad06
gaamjaa <dlgodus21@gmail.com>
2022-08-24 15:07:50 +0900

    update Home.js

--- src/Components/Tags_hy.js
+++ src/Components/Tags_hy.js
@@ -5,24 +5,25 @@ function Tags({id, name, letters, color}) {
     return(
         <div className="effect">
             {
-                id === -1 ? 
+                id === -1 ?
+                <Link to={`/newTag`}>
                 <button className="w-32 h-20 bg-[#BABABA] m-3 font-bold rounded-md shadow-md">
-                    <Link to={`/newTag`}>
                         <span className="flex justify-center">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M2 12.1074H22M12 2V22" stroke="white" stroke-width="4" stroke-linecap="round"/>
                         </svg>
                         </span>
-                    </Link>
                 </button>
+                </Link>
                 :
+                <Link to={`/tags/${id}`}>
                 <button 
                     style={{backgroundColor: color}}
                     className="grid place-items-start w-32 h-20 m-3 p-2 font-bold rounded-md shadow-md buri text-white text-sm"
                 >
-                    <Link to={`/tags/${id}`}>#{name}</Link>
+                    #{name}
                 </button>
-
+                </Link>
             }
         </div>
     );
