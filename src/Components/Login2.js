1b07e5ba84a3dc78f58876c9d11822922335655e
jjeongsu <92345556+jjeongsu@users.noreply.github.com>
2022-08-25 22:32:44 +0900

    Update Login2.js

--- src/Components/Login2.js
+++ src/Components/Login2.js
@@ -20,7 +20,7 @@ const Login2 = () => {
   };
 
   const signUp = () => {
-    fetch("www.duke0410.shop/user/login", {
+    fetch("https://www.duke0410.shop/user/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
