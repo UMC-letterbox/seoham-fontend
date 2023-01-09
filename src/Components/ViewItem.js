1a8da98bf9ffea43b1b0ee240023115e5107a9b7
ariling <97501226+Ariling@users.noreply.github.com>
2023-01-09 12:16:36 +0900

    다중태그에 따른 UI 수정

--- src/Components/ViewItem.js
+++ src/Components/ViewItem.js
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
-const ViewItem = ({ postIdx, tagName, date, tagColor, tagIdx }) => { //tagColor, tagIdx 추가로 얻기
+const ViewItem = ({ postIdx, tagName, date, tagColor, tagIdx }) => {
+  //tagColor, tagIdx 추가로 얻기
   const navigate = useNavigate();
   //const tagIdx = 0; // 임시
   const getTag = () => {
@@ -9,16 +10,31 @@ const ViewItem = ({ postIdx, tagName, date, tagColor, tagIdx }) => { //tagColor,
   const strDate = new Date(date).toLocaleDateString();
   return (
     <div>
-      <div>
-        <button
-          onClick={getTag}
-          class={`h-28 w-36 m-3 float-left cursor-pointer rounded-xl px-5 py-3 bg-[${tagColor}] text-white shadow-md`}
-        >
-          <div class="text-left py-2">#{tagName}</div>
-          <br></br>
-          <div class="text-right">{strDate}</div>
-        </button>
-      </div>
+      {tagName.length == 1 ? (
+        <div>
+          <button
+            onClick={getTag}
+            class={`h-28 w-36 m-3 float-left cursor-pointer rounded-xl px-5 py-3 bg-[${tagColor}] text-white shadow-md`}
+          >
+            <div class="text-left">#{tagName}</div>
+            <br></br>
+            <div class="text-right">{strDate}</div>
+          </button>
+        </div>
+      ) : (
+        <div>
+          <button
+            onClick={getTag}
+            class={`h-28 w-36 m-3 float-left cursor-pointer rounded-xl px-5 py-3 bg-[${tagColor[0]}] text-white shadow-md`}
+          >
+            <div class="text-left">
+              #{tagName[0]} 외 {tagName.length - 1}개 태그
+            </div>
+            <br></br>
+            <div class="text-right">{strDate}</div>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
