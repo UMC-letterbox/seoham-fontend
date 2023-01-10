24903174773a8339b63237fcedf138291014fa0f
jjeongsu <92345556+jjeongsu@users.noreply.github.com>
2023-01-10 10:35:54 +0900

    Update View_letter.js

--- src/Pages/View_letter.js
+++ src/Pages/View_letter.js
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import Menubox from "../Components/Menubox";
 import * as Data from "../getTags";
 import "../css/font.css";
+import Tag from "./Tag";
 function View_letter() {
   //param 받기. 편지보는 버튼에서 연결시키기 - 지금은 임의로 확인하기 위해 프로필이랑 연결함
   const useParam = useParams();
@@ -10,21 +11,12 @@ function View_letter() {
   const tagId = useParam.tagId;
   const postId = useParam.id;
   const [theme, setTheme] = useState(localStorage.getItem("theme"));
+  let tags = [];
   useEffect(() => {
     localStorage.setItem("theme", theme);
     document.body.className = theme;
   }, [theme]);
-  console.log("id체크", tagId, postId);
-  // const info = {day: '2022.07.30', writer:'해연', tag:'HBD', tagColor:'rgb(253,164,175)',
-  //             content:"생일 축하해! 오늘 맛있는 거 많이 먹고 즐거운 하루 됐으면 좋겠다! 생일 축하해! 오늘 맛있는 거 많이 먹고 즐거운 하루 됐으면 좋겠다!생일 축하해! 오늘 맛있는 거 많이 먹고 즐거운 하루 됐으면 좋겠다!생일 축하해! 오늘 맛있는 거 많이 먹고 즐거운 하루 됐으면 좋겠다!생일 축하해! 오늘 맛있는 거 많이 먹고 즐거운 하루 됐으면 좋겠다!생일 축하해! 오늘 맛있는 거 많이 먹고 즐거운 하루 됐으면 좋겠다!생일 축하해! 오늘 맛있는 거 많이 먹고 즐거운 하루 됐으면 좋겠다!생일 축하해! 오늘 맛있는 거 많이 먹고 즐거운 하루 됐으면 좋겠다!생일 축하해! 오늘 맛있는 거 많이 먹고 즐거운 하루 됐으면 좋겠다!생일 축하해! 오늘 맛있는 거 많이 먹고 즐거운 하루 됐으면 좋겠다!"}
-
-  // const getData = Data.getLetter(tagId, id);
-  // const letter = getData.letter;
-  // const tag = getData.tag;
-  // console.log('편지 확인 ', getData.letter, letter, 'tag확인', getData.tag, tag);
-  //console.log(Data.getLetter(tagId, id));
-
-  // 14~17번째 줄에서 api로 대체하기
+  // console.log("id체크", tagId, postId);
   let data = [];
   useEffect(() => {
     fetch(`https://seohamserver.shop/posts/${postId}`, {
@@ -42,10 +34,7 @@ function View_letter() {
         setPost(thisPost);
       });
   }, []);
-
-  // letterId, content, date, sender, tagName 정보 획득
-  //
-
+  
   const [isopen, setOpen] = useState(false);
   const navigate = useNavigate();
 
@@ -88,6 +77,33 @@ function View_letter() {
     navigate(-1);
   };
 
+  const TagView = (i) =>{
+    console.log("tagView 실행");
+    console.log(post.tagName[i]);
+    return(
+      <span 
+        //style={{backgroundColor: tColor}}
+        className=" px-6 py-2 rounded-full shadow-lg text-white">
+          {post?.tagName[i]}
+      </span>
+    )
+  }
+
+  // function makeTagList(thisPost){
+
+  //   console.log(thisPost?.tagName[0]);
+  //   let le = thisPost?.tagColor.length;
+  //   console.log(le, "length");
+  //   for(let i = 0;i <=le; i++){
+  //     tags.push({
+  //       name: thisPost?.tagName[i],
+  //       color: thisPost?.tagColor[i]
+  //     });
+  //   }
+  //   console.log(tags);
+  //   console.log("taglist 함수호출됨");
+  // }
+
   return (
     <div className={`${theme} h-screen`}>
       <div className="flex justify-between p-5">
@@ -118,16 +134,22 @@ function View_letter() {
           {post?.content}
         </div>
       </div>
-      <div className="flex justify-between mt-5 mx-10">
-        <span className="bg-rose-300 px-6 py-2 rounded-full shadow-lg text-white">
-          {/*style={{backgroundColor: info.tagColor}} className='rounded'>*/}#
-          {post?.tagName}
-        </span>
+      <div className="flex justify-end mt-5 mx-10">
         <span className="text-rose-300 border-y-2 border-rose-300 py-2">
           <span>날짜: {post?.date.slice(0, 10)}&nbsp;</span>
         </span>
       </div>
+      <div className="flex justify-start mt-2 mx-10">
+        {post?.tagName.map((t, index)=>( 
+          <span 
+            style={{backgroundColor: post?.tagColor[index]}}
+            className=" px-6 py-2 rounded-full shadow-lg text-white m-2">
+              {post?.tagName[index]}
+          </span>))}
+      </div>
+      
     </div>
+
   );
 }
 
