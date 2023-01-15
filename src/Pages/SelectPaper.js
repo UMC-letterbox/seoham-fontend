a2b5e4e339830802ff2f16f5cb1b1b85bec31e8a
jjeongsu <92345556+jjeongsu@users.noreply.github.com>
2023-01-15 10:59:35 +0900

    Update SelectPaper.js

--- src/Pages/SelectPaper.js
+++ src/Pages/SelectPaper.js
@@ -3,20 +3,27 @@ import "../App.css";
 
 const LetterPaper = ({setSelected, id, src}) => {
     return (
-        <div className="flex flex-col items-center w-5/12 h-5/12 my-3" >
-            <button 
+        <button 
+            className="flex flex-col items-center w-5/12 h-5/12 my-1" 
             onClick= {(e)=> {
                 console.log('선택된 편지지: ', e.target.value);
-                console.log(e.target);
-                e.target.focus({focusVisible: true});
                 setSelected(e.target.value);
             }}
-            className="w-20 h-20 rounded-md paperBtn"
-            style={{background: `url(${src})`}}
-            value = {id} >
-            </button>
-            <span className="text-sm buri"> 편지지{id}</span>
-        </div>
+            value = {id}
+            >
+            <label className="w-20">
+                <input
+                    className="radio-btn w-20"
+                    type="radio"
+                    value={id}
+                    name="paper"/>
+                <div 
+                className="w-20 h-20 rounded-md paperBtn"
+                style={{background: `url(${src})`}} >
+                </div>
+                <span className="text-sm buri text-black"> 편지지{id}</span>
+            </label>
+        </button>
     )
 };
 
@@ -24,27 +31,27 @@ const LetterPaper = ({setSelected, id, src}) => {
 export const papers = [
     {
         id: 1,
-        src: "img/paper1.jpg"
+        src: "/img/paper1.jpg"
     },
     {
         id: 2,
-        src: "img/paper2.jpg"
+        src: "/img/paper2.jpg"
     },
     {
         id: 3,
-        src: "img/paper3.jpg"
+        src: "/img/paper3.jpg"
     },
     {
         id: 4,
-        src: "img/paper4.jpg"
+        src: "/img/paper4.jpg"
     },
     {
         id: 5,
-        src: "img/paper5.jpg"
+        src: "/img/paper5.jpg"
     },
     {
         id: 6,
-        src: "img/paper6.jpg"
+        src: "/img/paper6.jpg"
     },
 ];
 
@@ -52,11 +59,12 @@ export const papers = [
 
 const SelectPaper = ({setSelected, selected, setIsOpen}) => {
     return (
-        <div>
-            <header>
-                <h1 className="text-center font-bold text-lg buri mt-5 mb-3">편지지 선택</h1>
+        <div className="flex-col justify-center">
+            <header className=" text-black w-20 ml-12 mt-5 mb-3">
+                    편지지 선택
             </header>
             <div className="flex flex-wrap justify-center items-center">
+                
                 {
                     papers.map((paper) => (
                         <LetterPaper key={paper.id} id={paper.id} src= {paper.src} setSelected={setSelected}/>
@@ -66,7 +74,7 @@ const SelectPaper = ({setSelected, selected, setIsOpen}) => {
             
             <div className = "flex justify-center items-center">
             <button
-                className="mt-3 bg-red-400 flex justify-center items-center decoration-white w-28  h-10 text-center font-semibold rounded-full text-slate-50"
+                className="mt-3 bg-red-400 flex justify-center items-center decoration-white w-32  h-10 text-center font-semibold rounded-full text-slate-50 "
                 onClick ={() => {setIsOpen(false)}}
             > 편지지 선택완료</button>
             </div>
